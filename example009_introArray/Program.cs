aba5de214c9b28c199ffead6991979feed5fb104
Jane <evgenya.mun@yandex.ru>
2022-04-15 17:52:40 +0300

    add array task

---
+++ example009_introArray/Program.cs
@@ -0,0 +1,19 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+} // первый элемент имеет индекс 0, следующий через 1, 2, 3, и тд
+//              0  1   2   3   4   5   6    7   8
+int[] array = {11, 211, 31, 41, 15, 61, 17, 18, 19};
+//array[0]=12; //нулевому(0) значению массива присвоить число 12. сейчас 11. каким образом можно обратиться к массиву и записать в него знаяение
+//Console.WriteLine(array[4]); //показываю каким образом можно обратиться к массиву и получить значение соответствующего эллемента по указанному индексу
+
+int result = Max(                    //функция
+    Max(array[0], array[1], array[2]), 
+    Max(array[3], array[4], array[5]), 
+    Max(array[6], array[7], array[8])); 
+
+Console.WriteLine(result);
+//поиск максимума из 9
--- метод_функция/Program.cs
+++ метод_функция/Program.cs
@@ -20,7 +20,7 @@ int c3 = 33;
 //int max2 = Max(a2, b2, c2);
 //int max3 = Max(a3, b3, c3);
 
-int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3)); // тоже самое,что выше
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3)); // тоже самое,что выше - функциональное программирование
 
 Console.WriteLine(max);
 
